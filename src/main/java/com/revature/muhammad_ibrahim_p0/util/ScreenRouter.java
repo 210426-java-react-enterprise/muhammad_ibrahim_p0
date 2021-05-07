d7014b4dc00a1af908db6e1741ac06c4e3057455
myibrahim36 <68923981+myibrahim36@users.noreply.github.com>
2021-05-07 18:26:30 -0400

    created util package and added classes AppState, LinkedList, ScreenRouter. They're all empty

---
+++ src/main/java/com/revature/muhammad_ibrahim_p0/util/ScreenRouter.java
@@ -0,0 +1,10 @@
+/**
+ * ScreenRouter: This holds a linked list of all screens and would have a method that can be called to navigate
+ * between screens and it gives a pre-made screen.
+ *
+ * */
+
+package com.revature.muhammad_ibrahim_p0.util;
+
+public class ScreenRouter {
+}
