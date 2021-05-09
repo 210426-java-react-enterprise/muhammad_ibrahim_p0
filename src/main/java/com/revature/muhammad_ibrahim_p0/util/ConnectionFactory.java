5028bb419fbfee70801cae982d19a87f672558b8
myibrahim36 <68923981+myibrahim36@users.noreply.github.com>
2021-05-09 11:17:52 -0400

    started working on creating a connection factory to connect to db.. Stopped to watch video to get help

---
+++ src/main/java/com/revature/muhammad_ibrahim_p0/util/ConnectionFactory.java
@@ -0,0 +1,25 @@
+package com.revature.muhammad_ibrahim_p0.util;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConnectionFactory {
+
+    private static ConnectionFactory connectionFactory;
+    private Properties properties = new Properties();
+
+    static {
+        try{
+            Class.forName("org.postgresql.Driver");
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    private ConnectionFactoey() {
+        try {
+            properties.load(new FileReader());
+        }
+    }
+
+}
