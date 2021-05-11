589608c98397248f810433d60cefee79991742b6
myibrahim36 <68923981+myibrahim36@users.noreply.github.com>
2021-05-11 17:50:13 -0400

    building skeleton for my dashboard

--- src/main/java/com/revature/muhammad_ibrahim_p0/screens/DashboardScreen.java
+++ src/main/java/com/revature/muhammad_ibrahim_p0/screens/DashboardScreen.java
@@ -1,4 +1,25 @@
 package com.revature.muhammad_ibrahim_p0.screens;
 
-public class DashboardScreen {
+import com.revature.muhammad_ibrahim_p0.util.ScreenRouter;
+
+import java.io.BufferedReader;
+
+public class DashboardScreen extends Screen{
+
+    private BufferedReader consoleReader;
+    private ScreenRouter router;
+
+    public DashboardScreen (BufferedReader consoleReader, ScreenRouter router) {
+        super("DashboardScreen", "/dashboard");
+        this.consoleReader = consoleReader;
+        this.router = router;
+    }
+
+    public void render() {
+
+        String firstname;
+        String lastname;
+
+
+    }
 }
